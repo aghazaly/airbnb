e3f282967502dd88e8aa3abefaff961b078892c5
aghazaly <ahmed.ghazaly.1@gmail.com>
2020-04-12 14:43:43 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='airbnb_data',
+      version='0.1',
+      description='airbnb data',
+      packages=['airbnb_data'],
+      zip_safe=False,
+      include_package_data=True)
