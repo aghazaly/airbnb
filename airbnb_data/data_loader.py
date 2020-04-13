4d94d53404aa18ac75206692f923fbc28db21ba6
aghazaly <ahmed.ghazaly.1@gmail.com>
2020-04-13 10:30:04 +0100

    adding typing, changed argument na name

--- airbnb_data/data_loader.py
+++ airbnb_data/data_loader.py
@@ -3,17 +3,18 @@ This model is used to instantiate a data object for data ingestion
 """
 import os
 import glob
+import typing
 import pandas as pd
 
 class Data:
     """
     Target is a target column name
     """
-    def __init__(self, target):
+    def __init__(self, target: str):
         self.target = target #target column name
         self.file_path = __file__ #path to the folder where this module is located
 
-    def load_data(self):
+    def load_data(self) -> typing.Dict:
         """
         This function returns a dictionary of dataframes
         """
@@ -23,20 +24,22 @@ class Data:
         for path in abs_paths:
             folder_name = path.split("/")[-2]
             file_name = path.split("/")[-1]
-            data_dict[f"{folder_name}_{file_name.split('.')[0]}"] = pd.read_csv(path) #append df to a dict
+            #append df to a dict
+            data_dict[f"{folder_name}_{file_name.split('.')[0]}"] = pd.read_csv(path)
         return data_dict
 
-    def prepare_data(self, df, na, features):
+    def prepare_data(self, df: pd.DataFrame, na_option: str, features: list) -> pd.DataFrame:
         """
         Takes a dataframe and returns cleaned dataframe
         """
         cleaned_df = df.dropna(subset=[self.target])
-        if na == 'drop_na':
+        if na_option == 'drop_na':
             cleaned_df = cleaned_df.dropna(subset=features)
-        elif na == 'mean':
+        elif na_option == 'mean':
             cleaned_df[features] = cleaned_df[features].apply(lambda x: x.fillna(x.mean()))
         else:
-            percentage_na = cleaned_df.isna().mean() #number of rows with na divided by total n number of rows
+            #number of nan rows divided by total number of rows
+            percentage_na = cleaned_df.isna().mean()
             na_cols = percentage_na[percentage_na > 0].index.tolist()
 
             print(f"these columns still have NaNs {na_cols}")
